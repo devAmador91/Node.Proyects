569fa956ca7921dd6f8da2fbfb58fc8af0bf7cf3
RepasoHenry <devWarrior7@protonmail.com>
2022-06-30 19:43:20 -0500

    Funcionalidades ok

--- ClimaApp/app.js
+++ ClimaApp/app.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
-const { inquiererMenu, pausa, leerInput, opcionLugares } = require("./helperes/inquirer");
+const {
+  inquiererMenu,
+  pausa,
+  leerInput,
+  opcionLugares,
+} = require("./helperes/inquirer");
 const Busquedas = require("./helperes/models/busquedas");
+require("colors");
 
 const main = async () => {
   console.clear();
@@ -14,21 +20,37 @@ const main = async () => {
         const lugar = await leerInput();
         //Buscar los lugares
         const lugares = await busquedas.ciudad(lugar);
+        if(!lugares.length){
+          console.log("No se encontro la ciudad ingresada".red)
+          break;
+        }
+  
         //Seleccionar lugar
-        const {Ciudad, Longitud, Latitud} = await opcionLugares(lugares);
+        const { Ciudad, Longitud, Latitud } = await opcionLugares(lugares);
+        busquedas.registroCiudades(Ciudad);
         //Datos del clima
+        const { temperatura, min, max, descripcion } = await busquedas.clima(
+          Longitud,
+          Latitud
+        );
         //Mostrar Resultados
         console.log("\nInformación de la ciudad\n".green);
         console.log(`Ciudad: ${Ciudad}`);
         console.log(`Lat: ${Latitud}`);
         console.log(`Lng: ${Longitud}`);
-        console.log("Temperatura");
-        console.log("Minima:");
-        console.log("Maxima:");
+        console.log(`Temperatura: ${temperatura}`);
+        console.log(`Minima: ${min}`);
+        console.log(`Maxima: ${max}`);
+        console.log(`Descripcion: ${descripcion}`);
         break;
       case 2:
-        console.log("Opcion 2");
-        break;
+        if(busquedas.historialBusquedas){
+          busquedas.historialBusquedas.map((c,i)=>{
+            console.log(`${i+1}`.green, `${c}`);
+          })
+          break;
+        }
+       break;
     }
     await pausa();
   } while (opt !== 0);
--- ClimaApp/helperes/models/busquedas.js
+++ ClimaApp/helperes/models/busquedas.js
@@ -1,11 +1,13 @@
 const axios = require("axios");
-const { MAPBOX_KEY } = process.env;
+const { MAPBOX_KEY, OPENWEATHER } = process.env;
+const fs = require("fs");
+
 
 class Busquedas {
-  historial = ["Tegucigalpa", "México", "Colombia", "Cuba"];
+  historial = [];
 
   constructor() {
-    //Lerr Db si existe
+    this.historialBusquedas
   }
 
   get paramsMapBox() {
@@ -15,7 +17,7 @@ class Busquedas {
       language: "es",
     };
   }
-//busqueda
+  //busqueda
   async ciudad(lugar = "") {
     try {
       const instancia = axios.create({
@@ -38,6 +40,58 @@ class Busquedas {
       return [];
     }
   }
+
+  get paramsClima() {
+    return {
+      appid: OPENWEATHER,
+      units: "metric",
+      lang: "es",
+    };
+  }
+
+  async clima(lon, lat) {
+    try {
+      const instancia = axios.create({
+        baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+        params: { ...this.paramsClima, lon, lat },
+      });
+      const response = await instancia.get();
+      const { main, weather } = response.data;
+      return {
+        temperatura: main.temp,
+        min: main.temp_min,
+        max: main.temp_max,
+        descripcion: weather[0].description,
+      };
+    } catch (error) {
+      console.log(error);
+      return {};
+    }
+  }
+
+  get historialBusquedas() {
+    if(!fs.existsSync("./registro/data.json")){
+      return [];
+    };
+
+    const data = fs.readFileSync("./registro/data.json", { encoding: "utf-8" });
+    const convert = JSON.parse(data);
+    this.historial = convert;
+    return this.historial;
+  }
+
+  registroCiudades = (registro) => {
+    const data = this.historialBusquedas;
+    
+    if (data.length && data.includes(registro)) {
+      return null;
+    }
+    this.historial.unshift(registro);
+    if(this.historial.length > 5){
+      this.historial.pop();
+    }
+    fs.writeFileSync("./registro/data.json", JSON.stringify(this.historial));
+  };
 }
 
 module.exports = Busquedas;
